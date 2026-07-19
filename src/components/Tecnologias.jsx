7cd11bfb3be3d012fd4a88b9d1cf93ff2fb7cb62
carlosprogramador2026 <burlador2024top@gmail.com>
2026-07-19 07:35:03 -0300

    Adiciona o fundo de codigo vazado tambem na secao abaixo de "Diferenciais", deixando as duas secoes do arco brilhante identicas
    Tecnologias agora usa CodeBackdrop + GlowArc, igual a secao abaixo de
    "Depoimentos" (CtaRoxo) -- mesma animacao nos dois lugares.

--- src/components/Tecnologias.jsx
+++ src/components/Tecnologias.jsx
@@ -1,8 +1,10 @@
 import GlowArc from "./GlowArc"
+import CodeBackdrop from "./CodeBackdrop"
 
 export default function Tecnologias() {
   return (
     <section id="tecnologias" className="tecnologias-section">
+      <CodeBackdrop />
       <GlowArc />
     </section>
   )
